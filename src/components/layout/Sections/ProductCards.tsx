b2bb3e23fcdce043bfb64e4b873a4e4d334aa098
Dragos Micu <hello@wpharvest.com>
2025-10-11 20:59:52 +0300

    Updated images quality

--- src/components/layout/Sections/ProductCards.tsx
+++ src/components/layout/Sections/ProductCards.tsx
@@ -16,7 +16,7 @@ export default function ProductCards({ data }: ProductCardsProps) {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
           {data.products.map((product, index) => (
             <div key={index} className="">
-              <Image src={product.image.node.sourceUrl} alt={product.title} width={440} height={440} className="w-full h-auto rounded-3xl" />
+              <Image src={product.image.node.sourceUrl} alt={product.title} width={440} height={440} quality={100} className="w-full h-auto rounded-3xl" />
               <div className="flex flex-col gap-2 lg:px-4 pt-4">
                 <h3 className="text-2xl">{product.title}</h3>
                 <span className="price-el text-[#343C50] text-sm" dangerouslySetInnerHTML={{ __html: product.price }} />
