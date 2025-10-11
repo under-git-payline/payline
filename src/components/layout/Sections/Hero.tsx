79ed8a9e7e20132912df3d1edb3be409062dc253
Dragos Micu <hello@wpharvest.com>
2025-10-11 22:00:37 +0300

    Fixed iframe responsiveness

--- src/components/layout/Sections/Hero.tsx
+++ src/components/layout/Sections/Hero.tsx
@@ -68,8 +68,8 @@ export default function Hero({ data }: HeroProps) {
   }, [shouldShowForm, data?.formId]);
 
   return (
-    <div className={`relative container ${hasRightSide ? 'flex flex-col-reverse lg:flex-row items-center justify-between' : 'flex flex-col items-center justify-center text-center'} gap-10 pt-10 px-10 lg:py-10 bg-brand-gradient text-white rounded-4xl ${data?.addCalculator ? 'mb-50' : 'mb-10'}`}>
-      <div className={`flex flex-col gap-10 ${hasRightSide ? 'lg:max-w-1/2' : 'max-w-3xl w-full items-center text-center'} pb-10 lg:p-10`}>
+    <div className={`relative container ${hasRightSide ? `flex ${hasImage ? 'flex-col-reverse' : 'flex-col'} lg:flex-row items-center justify-between` : 'flex flex-col items-center justify-center text-center'} gap-10 pt-10 px-10 lg:py-10 bg-brand-gradient text-white rounded-4xl ${data?.addCalculator ? 'mb-50' : 'mb-10'}`}>
+      <div className={`flex flex-col gap-10 ${hasRightSide ? 'lg:max-w-1/2' : 'max-w-3xl w-full items-center text-center'} ${hasImage ? 'pb-10' : ''} lg:p-10`}>
         <div className="flex flex-col gap-3">
           {heroData.tag && (
             <span className={`text-sm/6 font-light py-1 px-3 bg-black/10 rounded-sm ${hasRightSide ? 'w-fit' : 'mx-auto'}`}>
@@ -96,7 +96,7 @@ export default function Hero({ data }: HeroProps) {
         </div>
       </div>
       {shouldShowForm ? (
-        <div className="w-full lg:w-auto" ref={formContainerRef}>
+        <div className="w-full lg:w-auto pb-10 lg:p-0" ref={formContainerRef}>
           <div id="hero-hubspot-form" className="bg-white text-black rounded-2xl p-4 sm:p-6 md:p-8 w-full lg:w-[620px] max-w-[620px]" />
           <Script
             src="https://js.hsforms.net/forms/v2.js"
@@ -108,11 +108,11 @@ export default function Hero({ data }: HeroProps) {
           />
         </div>
       ) : shouldShowIframe ? (
-        <div className="w-full lg:w-auto bg-white rounded-2xl overflow-hidden">
+        <div className="w-full bg-white rounded-2xl overflow-hidden mb-10 lg:m-0">
           <iframe
             ref={iframeRef}
             src={data?.iframeUrl}
-            className="w-full lg:w-[620px] max-w-[620px] rounded-2xl border-0"
+            className="w-full max-w-[620px] rounded-2xl border-0"
             style={{ 
               height: '980px'
             }}
