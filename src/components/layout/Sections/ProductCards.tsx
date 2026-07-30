d5ce6072dafc6d6c875961291da5977d74ae0055
Dragos Micu <hello@wpharvest.com>
2026-07-30 22:29:47 +0300

    Hotfix.

--- src/components/layout/Sections/ProductCards.tsx
+++ src/components/layout/Sections/ProductCards.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Button from "../../ui/Button";
 import { ProductCardsLayoutData, FlexibleContentProps } from "@/types/acf";
 import Link from "next/dist/client/link";
 
